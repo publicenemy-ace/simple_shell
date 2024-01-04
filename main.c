3f40b0908ba008269706c9fcd22ffb0e1df38172
publicenemy-ace <niyisatun@yahoo.com>
2024-01-04 02:55:49 -0800

    simple shell

--- main.c
+++ main.c
@@ -56,10 +56,10 @@ int run(char **args, char **front)
 		{
 			execve(command, args, environ);
 			if (errno == EACCES)
-				ret = (create_error(args, 126));
+				ret = (make_error(args, 126));
 			free_env();
 			free_args(args, front);
-			free_alias_list(aliases);
+			setfree_alias_list(aliases);
 			_exit(ret);
 		}
 		else
@@ -97,9 +97,9 @@ int main(int argc, char *argv[])
 
 	if (argc != 1)
 	{
-		ret = proc_file_commands(argv[1], exe_ret);
+		ret = process_file_cmd(argv[1], exe_ret);
 		free_env();
-		free_alias_list(aliases);
+		setfree_alias_list(aliases);
 		return (*exe_ret);
 	}
 
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
 		while (ret != END_OF_FILE && ret != EXIT)
 			ret = handle_args(exe_ret);
 		free_env();
-		free_alias_list(aliases);
+		setfree_alias_list(aliases);
 		return (*exe_ret);
 	}
 
@@ -121,12 +121,12 @@ int main(int argc, char *argv[])
 			if (ret == END_OF_FILE)
 				write(STDOUT_FILENO, new_line, 1);
 			free_env();
-			free_alias_list(aliases);
+			setfree_alias_list(aliases);
 			exit(*exe_ret);
 		}
 	}
 
 	free_env();
-	free_alias_list(aliases);
+	setfree_alias_list(aliases);
 	return (*exe_ret);
 }
